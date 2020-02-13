fe4eefdecfbab6536196f8c7632b6e21f9cdd4b2
Pooi lai Choong <choongpl@gmail.com>
2020-02-13 15:20:52 +1300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,94 +1,107 @@
 console.log('json & ajax');
 
-// // to test jquery
-// $(document).ready(function(){
-//   $('body').click(function(){
-//     $(this).css('color','blue');
-//   });
+$(document).ready(function(){
+
+//accessing key from json file
+var myKey = JSON.parse(apiKey);
+console.log(myKey[0]);
+myKey = myKey[0].key;
+
+
+var endPoint;
+var size;
+
+//reading users choice
+document.getElementById('submit').addEventListener('click', function(){
+endPoint = document.getElementById('endpoints').value;
+size = document.getElementById('sizes').value;
+console.log(endPoint, size);
+displayData(endPoint, size);
+});
+//
+//
 // });
-//
-// // Javascript data - array of objects
-// var jsData = [
-//   {
-//     name : 'Peter',
-//     age : 50
-//   },
-//   {
-//     name : 'John',
-//     age : 25
-//   }
-// ];
-//
-// console.log(jsData);
-//
-// //displaying js objects in the browser window
-// var i;
-// for(i=0; i<jsData.length; i++) {
-// document.getElementById('result').innerHTML +=
-//   '<h1>' + jsData[i].name + ' is ' +
-//   jsData[i].age + ' is years old</h1>';
-// };
 
-// // =============================================================
-// // jsonData
-// var jsonData = [
-//   {
-//     "name" : "Lucy",
-//     "age" : 12
-//   },
-//   {
-//     "name" : "Mark",
-//     "age" : 78
-//   }
-// ];
-//
-// console.log(jsonData);
-//
-// //displaying json objects in the browser window
-// var j;
-// for(j=0; j<jsonData.length; j++) {
-// document.getElementById('result').innerHTML +=
-//   '<h1>' + jsonData[j].name + ' is ' +
-//   jsonData[j].age + ' is years old</h1>';
-// };
-
-// // =============================================================
-// // retrieving json data from an external file
-// var person = JSON.parse(myData);
-// console.log(person);
-//
-//
-// //displaying json objects in the browser window
-// var k;
-// for(k=0; k<person.length; k++) {
-// document.getElementById('result').innerHTML +=
-//   '<h1>' + person[k].name + ' is ' +
-//   person[k].age + ' is years old</h1>';
-// };
+function displayData(ep, si){
+  console.log(ep, si);
 
 
-// ===========================================================
 //ajax method
 $.ajax({
-  url:'js/mock_data.json',
+  // url:'https://api.unsplash.com/search/collections?page=1&query=office&client_id=bb780e16ef58132ea59a6beae18cf0f0917c20802cd4def27699e9939eb3b1b1',
+  url:`https://api.unsplash.com/${ep}/?client_id=${myKey}`,
   type: 'GET',
   data: 'json',
   success: function(data){
     console.log(data);
-    var i;
-    for (i=0; i<data.length; i++){
-      document.getElementById('result').innerHTML +=
-      '<div class="col col-sm-6 col-md-6 col-lg-4 mx-5 my-5">' +
-      '<h1>' + data[i].first_name +
-       data[i].last_name + '</h1>' +
-      '<h2>' + data[i].gender + '</h2>' +
-      '<h3>' + data[i].email + '</h3>' +
-      '<img class="img-thumbnail" src="' + data[i].image + '" alt="Avatar">' +
-      '</div>';
-    }
+    // if (ep === 'collections'){
+    //   collections(data,ep, si);
+    // } else if (ep === 'photos'){
+    //   photos(data, ep, si);
+    // }
+    // function collections(d, e,s){
+    //   var k;
+    //   var userSize;
+    //
+    //     document.getElementById('result').innerHTML = '';
+    //   for(k = 0; k < d.length; k++ ){
+    //     if (s === 'full') {
+    //       userSize = d[k].cover_photo.urls.full;
+    //     } else if (s === 'raw') {
+    //       userSize = d[k].cover_photo.urls.raw;
+    //     } else if (s === 'regular') {
+    //       userSize = d[k].cover_photo.urls.regular;
+    //     }else if (s === 'small') {
+    //       userSize = d[k].cover_photo.urls.small;
+    //     } else if (s === 'thumb') {
+    //       userSize = d[k].cover_photo.urls.thumb;
+    //     }
+    //
+    //
+    //     document.getElementById('result').innerHTML +=
+    //     '<div class="col">' +
+    //       '<img class="img-thumbnail" alt="Image" src="' + userSize + '">' +
+    //     '</div>';
+    //   }
+    // }
+    //
+    //
+    //   function photos(d, e,s){
+    //       var j;
+    //     var photoSize;
+    //       document.getElementById('result').innerHTML = '';
+    //   for(j = 0; j < d.length; j++ ){
+    //     if (s === 'full') {
+    //       photoSize = d[j].urls.full;
+    //     } else if (s === 'raw') {
+    //       photoSize = d[j].urls.raw;
+    //     } else if (s === 'regular') {
+    //       photoSize = d[j].urls.regular;
+    //     }else if (s === 'small') {
+    //       photoSize = d[j].urls.small;
+    //     } else if (s === 'thumb') {
+    //       photoSize = d[j].urls.thumb;
+    //     }
+    //
+    //
+    //     document.getElementById('result').innerHTML +=
+    //     '<div class="col">' +
+    //       '<img class="img-thumbnail" alt="Image" src="' + photoSize + '">' +
+    //     '</div>';
+    //   }
+    // }
+
+
+
+
+
   }, //success
   error: function(){
     console.log('error!');
   } //error
+
+}); //ajax
+
+}; // function displayData ends here
 
 }); //document.ready
